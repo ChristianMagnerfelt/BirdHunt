a4561b05e73dee28a863c88a5e877ab4fffe5df2
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2012-09-23 14:29:25 +0200

    Added skeleton

---
+++ main.cc
@@ -0,0 +1,28 @@
+#include "cplayer.h"
+#include "cclient.h"
+
+#include <iostream>
+
+int main(int pArgC,char **pArgs)
+{
+    if(pArgC<4)
+    {
+        std::cerr << "usage: " << pArgs[0] << " host port STANDALONE [options]" << std::endl;
+        return -1;
+    }
+
+    ducks::CPlayer lPlayer;
+    ducks::CClient lClient(lPlayer);
+
+    std::string lArgs=pArgs[3];
+
+    bool lStandalone=(lArgs=="STANDALONE");
+    
+    for(int i=4;i<pArgC;i++)
+    {
+        lArgs+=" ";
+        lArgs+=pArgs[i];
+    }
+    
+    lClient.Run(pArgs[1],pArgs[2],lArgs,lStandalone);
+}
